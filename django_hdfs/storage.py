08cf0ebd120ce11e8ced75b0df9b98362f8108ed
thanos vassilakis <thanos@syntazo.com>
2016-06-18 18:39:37 -0400

    Update storage.py

--- django_hdfs/storage.py
+++ django_hdfs/storage.py
@@ -1,48 +1,43 @@
- from django.conf import settings
-from django.core.files.storage import Storage
-from django.utils.six.moves.urllib.parse import urljoin
-from django.utils.deconstruct import deconstructible
-from django.core.files import File
-
-from hdfs import InsecureClient
-from hdfs.util import HdfsError
-
-import tempfile
-
 @deconstructible
 class HDFSStorage(Storage):
     """
     HDFS storage
     """
-    file_class = ContentFile
+
+    def fix_slashes(self, path):
+        sep = os.path.sep
+        if path[0] != sep:
+            path = sep + path
+        if path[-1] != sep:
+            path = path + sep
+        return path
+
     def __init__(self, location=None, base_url=None):
-        if location is None:
-            location = settings.MEDIA_ROOT
-        self.base_location = location
-        self.location = self.base_location
-        if base_url is None:
-            base_url = settings.MEDIA_URL
-        elif not base_url.endswith('/'):
-            base_url += '/'
-        self.base_url = base_url
-        self.client = InsecureClient(settings.HDFS_HOST)
+        self.hdfs_hosts = settings.HDFS_STROAGE['hosts']
+        self.hdfs_root = self.fix_slashes(settings.HDFS_STROAGE['root'])
+        self.media_root = settings.MEDIA_ROOT
+        self.media_url = self.fix_slashes(settings.MEDIA_URL)
+
+        self.fetch_url = '%s/webhdfs/v1%s%%s?op=OPEN' % (self.hdfs_hosts.split(',')[0], self.hdfs_root)
+        self.client = InsecureClient(self.hdfs_hosts)
 
     def _open(self, name, mode='rb'):
-        local_path=os.path.join(settings.MEDIA_ROOT, name)
-        print self.client.download(self.path(name), local_path=settings.MEDIA_ROOT, overwrite=True, temp_dir=tempfile.gettempdir())
+        local_path = os.path.join(settings.MEDIA_ROOT, name.replace('/', os.path.sep))
+        if not os.path.exists(local_path):
+            print self.client.download(self.path(name), local_path=os.path.dirname(local_path), overwrite=True,
+                                   temp_dir=tempfile.gettempdir())
         return File(open(local_path, mode))
 
     def _save(self, name, content):
         print "_save(%s, %s, %s)" % (self, name, content)
-        local_path=content.name
-        hdfs_path = self.path(name) #os.path.basename(local_path))
+        local_path = content.name
+        hdfs_path = self.path(name)  # os.path.basename(local_path))
         print hdfs_path, local_path
-        return self.client.upload(hdfs_path, local_path, overwrite=True)
+        self.client.write(hdfs_path, data=content, overwrite=True)
+        return name
 
     def url(self, name):
-        if self.base_url is None:
-            raise ValueError("This file is not accessible via a URL.")
-        return urljoin(self.base_url, filepath_to_uri(name))    
+        return self.fetch_url % name
 
     def delete(self, name):
         return self.client.delete(self.path(name))
@@ -67,4 +62,4 @@ class HDFSStorage(Storage):
             return False
 
     def path(self, name):
-        return self.location + name
+        return (self.hdfs_root + name).replace('\\','/')
